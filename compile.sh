22ceae8e3215b079617bff9d80f5fe79af4ccd19
Satyrn <pastawho@gmail.com>
2021-03-11 12:09:59 -0800

    Add files via upload

---
+++ compile.sh
@@ -0,0 +1,10 @@
+echo "Setting up environment..."
+rm -rf linux-tkg
+git clone https://github.com/Frogging-Family/linux-tkg.git
+rm -rf linux-tkg/linux-tkg-config/generic-desktop-profile.cfg
+cp config.cfg linux-tkg/linux-tkg-config/generic-desktop-profile.cfg
+cd linux-tkg
+echo "Beginning compilation..."
+makepkg -si
+sleep 1; rm -rf linux-tkg
+echo "Install finished!"
